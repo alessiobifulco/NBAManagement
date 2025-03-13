535498646a1f0e6d9faabf5f8c0e5d233aea344b
Alessio <alessiobifulco24@gmail.com>
2025-03-13 15:50:22 +0100

    mod: adjusted classes for use with database

--- APP/java/src/main/java/data/Contract.java
+++ APP/java/src/main/java/data/Contract.java
@@ -3,7 +3,7 @@ package data;
 import java.time.LocalDate;
 
 public class Contract {
-    
+
     int idContract;
     int idPlayer;
     int idTeam;
@@ -12,6 +12,7 @@ public class Contract {
     int years;
     boolean state;
 
+    // Costruttore senza idContract, che verrà assegnato automaticamente dal database
     public Contract(int idPlayer, int idTeam, int salary, LocalDate dateStart, int years, boolean state){
         this.idPlayer = idPlayer;
         this.idTeam = idTeam;
@@ -19,10 +20,62 @@ public class Contract {
         this.dateStart = dateStart;
         this.years = years;
         this.state = state;
-        this.idContract = idContract++;
     }
 
+    // Getter e Setter
+    public int getIdContract() {
+        return idContract;
+    }
 
+    public void setIdContract(int idContract) {
+        this.idContract = idContract;
+    }
 
-   
+    public int getIdPlayer() {
+        return idPlayer;
+    }
+
+    public void setIdPlayer(int idPlayer) {
+        this.idPlayer = idPlayer;
+    }
+
+    public int getIdTeam() {
+        return idTeam;
+    }
+
+    public void setIdTeam(int idTeam) {
+        this.idTeam = idTeam;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public LocalDate getDateStart() {
+        return dateStart;
+    }
+
+    public void setDateStart(LocalDate dateStart) {
+        this.dateStart = dateStart;
+    }
+
+    public int getYears() {
+        return years;
+    }
+
+    public void setYears(int years) {
+        this.years = years;
+    }
+
+    public boolean isState() {
+        return state;
+    }
+
+    public void setState(boolean state) {
+        this.state = state;
+    }
 }
--- APP/java/src/main/java/data/Player.java
+++ APP/java/src/main/java/data/Player.java
@@ -1,30 +1,93 @@
 package data;
 
 public class Player {
-    
+
     int idPlayer;
     String name;
     String surname;
-    enum Position {PG, SG, SF, PF, C};
+    public enum Position {PG, SG, SF, PF, C};
     int age;
     Position position;
-    enum Category {Superstar, AllStar, Bench, RolePlayer};
+    public enum Category {Superstar, AllStar, Bench, RolePlayer};
     Category category;
     int rank;
-    int expereince;
+    int experience;
     boolean freeAgent;
 
-
-    public Player(String name, String surname, int age, Position position, Category category, int rank, int expereince, boolean freeAgent){
+    // Costruttore senza idPlayer, che viene assegnato automaticamente dal database
+    public Player(String name, String surname, int age, Position position, Category category, int rank, int experience, boolean freeAgent){
         this.name = name;
         this.surname = surname;
         this.age = age;
         this.position = position;
         this.category = category;
         this.rank = rank;
-        this.expereince = expereince;
+        this.experience = experience;
         this.freeAgent = freeAgent;
-        this.idPlayer = idPlayer++;
     }
 
+    // Getter e Setter
+    public int getIdPlayer() {
+        return idPlayer;
+    }
+
+    public void setIdPlayer(int idPlayer) {
+        this.idPlayer = idPlayer;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    public boolean isFreeAgent() {
+        return freeAgent;
+    }
+
+    public void setFreeAgent(boolean freeAgent) {
+        this.freeAgent = freeAgent;
+    }
 }
